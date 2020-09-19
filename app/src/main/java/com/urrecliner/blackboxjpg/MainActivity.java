bc58934c5d327e9c4d63df666c8bea8b6eabe2e5
riopapa <riopapa@gmail.com>
2020-09-19 21:28:48 +0900

    Display progress

--- app/src/main/java/com/urrecliner/blackboxjpg/MainActivity.java
+++ app/src/main/java/com/urrecliner/blackboxjpg/MainActivity.java
@@ -2,14 +2,20 @@ package com.urrecliner.blackboxjpg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.SystemClock;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,16 +35,19 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     String PATH_PACKAGE = "BlackBox";
-    String PATH_EVENT_JPG = "eventJpg";
+    String PATH_EVENT_JPG = "jpgTemp";
     String PATH_EVENT_PHOTO = "eventPhoto";
     String DATE_PREFIX = "V";
     File mPackagePath = new File(Environment.getExternalStorageDirectory(), PATH_PACKAGE);
     File mPackageEventJpgPath = new File(mPackagePath, PATH_EVENT_JPG);
     File mPackageEventPhotoPath = new File(mPackagePath, PATH_EVENT_PHOTO);
     File [] eventFolders, eventPhotos;
+    int [] startPositions, finishPositions;
+    String [] eventNames;
     int folderCount, photoCount;
     boolean rtnCode;
     TextView textView;
+    StringBuilder folderSB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +58,34 @@ public class MainActivity extends AppCompatActivity {
         eventFolders = getDirectoryList(mPackageEventJpgPath);
         textView = findViewById(R.id.result);
         folderCount = eventFolders.length;
-        textView.setText((folderCount == 0) ? "No folders to convert" : "[ " + folderCount + " ] folders");
+        if (folderCount == 0)
+            textView.setText("No folders to convert");
+        else {
+            Arrays.sort(eventFolders);
+            startPositions = new int[folderCount];
+            finishPositions = new int[folderCount];
+            eventNames = new String[folderCount];
+            folderSB = new StringBuilder();
+            folderSB.append("Folder List\n");
+            for (int idx = 0; idx < folderCount; idx++) {
+                eventNames[idx] = eventFolders[idx].getName();
+                startPositions[idx] = folderSB.length();
+                folderSB.append(eventNames[idx])
+                        .append("\n");
+                finishPositions[idx] = folderSB.length();
+            }
+            folderSB.append("\n");
+            final String folderList = folderSB.toString();
+            this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText(folderList);
+                    textView.invalidate();
+                }
+            });
+        }
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -64,7 +98,8 @@ public class MainActivity extends AppCompatActivity {
         Intent intent;
         switch (item.getItemId()) {
             case R.id.action_run:
-                new Timer().schedule(new TimerTask() {
+                if (folderCount > 0)
+                    new Timer().schedule(new TimerTask() {
                     public void run() {
                         convert_photo();
                     }
@@ -75,68 +110,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void convert_photo() {
-        Log.w("event","Jpg Folder "+ folderCount);
-        if (folderCount > 0) {
-            Arrays.sort(eventFolders);
-            StringBuilder  folderSB = new StringBuilder();
-            folderSB.append("Folder List\n");
-            for (File eventPath: eventFolders) {
-                if (eventPath.getName().substring(0, 1).equals(DATE_PREFIX)) {  // to ignore . folder/files
-                    folderSB.append(eventPath.getName())
-                            .append("\n");
+        for (int idxPath = 0; idxPath < eventFolders.length; idxPath++) {
+            if (!eventNames[idxPath].substring(0,1).equals(DATE_PREFIX))
+                continue;
+            File eventPath = eventFolders[idxPath];
+            eventPhotos = getDirectoryList(eventPath);
+            File eventPhotoPath = new File(mPackageEventPhotoPath, eventNames[idxPath]);
+            if (!eventPhotoPath.exists())
+                rtnCode = eventPhotoPath.mkdirs();
+//            Arrays.sort(eventPhotos);
+            photoCount = eventPhotos.length;
+            for (int idxJpg = 0; idxJpg < eventPhotos.length; idxJpg++) {
+                File eventPhoto = eventPhotos[idxJpg];
+//                Log.w(eventPhoto.getName()," process");
+                int photoSize = (int) eventPhoto.length();
+                byte[] jpgBytes = new byte[photoSize];
+                try {
+                    BufferedInputStream buf = new BufferedInputStream(new FileInputStream(eventPhoto));
+                    buf.read(jpgBytes, 0, jpgBytes.length);
+                    buf.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            }
-            final String folderList = folderSB.toString()+"\n-->\n";
-            this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    textView.setText(folderList);
-                    textView.invalidate();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                Bitmap bm =BitmapFactory.decodeByteArray(jpgBytes, 0, jpgBytes.length);
+                Bitmap bm2 = bm.copy(Bitmap.Config.RGB_565, false);
+                bm2.compress(Bitmap.CompressFormat.JPEG, 90, stream);
+                byte[] photoBytes = stream.toByteArray();
+                FileOutputStream fileOutputStream = null;
+                try {
+                    fileOutputStream = new FileOutputStream(new File(eventPhotoPath, eventPhoto.getName()));
+                    fileOutputStream.write(photoBytes);
+                } catch (IOException e) {
+                    Log.e("Err1","IOException catch", e);
+                } finally {
+                    if (fileOutputStream != null) {
+                        try {
+                            fileOutputStream.close();
+                        } catch (IOException e) {
+                            Log.e("err 2", "IOException finally", e);
+                        }
+                    }
                 }
-            });
-
-            for (File eventPath: eventFolders) {
-                if (eventPath.getName().substring(0, 1).equals(DATE_PREFIX)) {  // to ignore . folder/files
-                    eventPhotos = getDirectoryList(eventPath);
-                    File eventPhotoPath = new File(mPackageEventPhotoPath, eventPath.getName());
-                    if (!eventPhotoPath.exists())
-                        rtnCode = eventPhotoPath.mkdirs();
-                    Arrays.sort(eventPhotos);
-                    photoCount = eventPhotos.length;
-                    int idx = 0;
-                    for (File eventPhoto: eventPhotos) {
-//                        Log.w(eventPhoto.getName()," process");
-                        int size = (int) eventPhoto.length();
-                        byte[] jpgBytes = new byte[size];
-                        try {
-                            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(eventPhoto));
-                            buf.read(jpgBytes, 0, jpgBytes.length);
-                            buf.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                        Bitmap bm =BitmapFactory.decodeByteArray(jpgBytes, 0, jpgBytes.length);
-                        Bitmap bm2 = bm.copy(Bitmap.Config.RGB_565, false);
-                        bm2.compress(Bitmap.CompressFormat.JPEG, 90, stream);
-                        byte[] photoBytes = stream.toByteArray();
-                        FileOutputStream fileOutputStream = null;
-                        try {
-                            fileOutputStream = new FileOutputStream(new File(eventPhotoPath, eventPhoto.getName()));
-                            fileOutputStream.write(photoBytes);
-                        } catch (IOException e) {
-                            Log.e("Err1","IOException catch", e);
-                        } finally {
-                            if (fileOutputStream != null) {
-                                try {
-                                    fileOutputStream.close();
-                                } catch (IOException e) {
-                                    Log.e("err 2", "IOException finally", e);
-                                }
-                            }
-                        }
-                        final String processMsg = folderList+eventPath.getName()+
-                                "\n" + eventPhoto.getName()+" ("+ ++idx + "/"+photoCount+")";
+                if (idxJpg % 10 == 0) {
+                    int bPos = startPositions[idxPath];
+                    int fPos = finishPositions[idxPath];
+                    int ePos = bPos + (fPos - bPos) * idxJpg / photoCount;
+//                    Log.w("pos ",bPos+"+ "+ePos);
+                    if (ePos > bPos) {
+                        SpannableString ss = new SpannableString(folderSB);
+                        ss.setSpan(new ForegroundColorSpan(Color.BLUE), bPos, fPos, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        ss.setSpan(new BackgroundColorSpan(Color.YELLOW), bPos, ePos, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        ss.setSpan(new StyleSpan(Typeface.BOLD), bPos, fPos, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        ss.setSpan(new RelativeSizeSpan(1.2f), bPos, fPos, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        final SpannableString processMsg = ss;
                         this.runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
@@ -144,13 +171,15 @@ public class MainActivity extends AppCompatActivity {
                                 textView.invalidate();
                             }
                         });
-                        SystemClock.sleep(100);
+//                        Log.w("prog disp " + idxJpg, eventPhoto.getName());
                     }
-                    File newFile = new File(eventPath.toString().replace(DATE_PREFIX+"20-","C20-"));
-                    rtnCode = eventPath.renameTo(newFile);
+                    SystemClock.sleep(100);
                 }
             }
+            File newFile = new File(eventPath.toString().replace(DATE_PREFIX+"20-","C20-"));
+            rtnCode = eventPath.renameTo(newFile);
         }
+
         this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -167,7 +196,31 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 3000);
 
+
     }
+
+
+    private SpannableString listUpFiles(int currIdx) {
+        int sPos = 0, fPos = 0;
+        StringBuilder sb = new StringBuilder();
+        sb.append("Folder List\n");
+        sPos = sb.length();
+        for (int idx = 0; idx < eventFolders.length; idx++) {
+            if (currIdx == idx)
+                sPos = sb.length();
+            String folderName = eventFolders[idx].getName();
+//            sb.append((currIdx == idx)? "<< ":"");
+            sb.append(folderName).append("\n");
+            fPos = sb.length();
+            sb.append("\n");
+        }
+        SpannableString ss = new SpannableString(sb);
+        ss.setSpan(new ForegroundColorSpan(Color.BLUE), sPos, fPos, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new StyleSpan(Typeface.BOLD), sPos, fPos, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(new RelativeSizeSpan(1.2f), sPos, fPos, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return ss;
+    }
+
     File[] getDirectoryList(File fullPath) {
         return fullPath.listFiles();
     }
